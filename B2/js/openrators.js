aae03c6b06af18a1b224e641598de30cc898fe74
ThanhTuyen0302 <doancaothanhtuyen0302gmail.com>
2024-10-27 11:33:16 +0700

    text

---
+++ B2/js/openrators.js
@@ -0,0 +1,20 @@
+//1. Toan tu so hoc (Arithmetic) (+-*/%)
+var a = 12 **2; // Luy thua
+console.log(a)
+// *** 1. tien to (prefix) (++a)
+console.log(++a); // 145
+console.log(a); // 145
+
+// *** 1. hau to (postfix) (a++)
+console.log(a++); // 144
+console.log(a); //145
+
+console.log(12 % 2); // chia Lay phan du
+console.log(12 / 2); // 6.5
+
+// 2. Toan tu gan (Assignment) (= -= += /= **m) ---------------
+var x = 10;
+x += 100; // x = x + 100;
+
+// 3. Toan tu so sanh (Comparison) (> < >= <= != == ===) --------------
+
